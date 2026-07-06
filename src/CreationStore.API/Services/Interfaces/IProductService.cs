dfd8c76a9ad6882f0cf618b3d5b90faf65f3f8ac
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-07-06 11:03:38 +0700

    add service interface for products

--- src/CreationStore.API/Services/Interfaces/IProductService.cs
+++ src/CreationStore.API/Services/Interfaces/IProductService.cs
@@ -4,8 +4,26 @@ namespace CreationStore.API.Services.Interfaces
 {
     public interface IProductService
     {
+        // Member functions 
         Task<List<ProductResponseDTO>> GetAllProductsAsync();
 
         Task<ProductResponseDTO?> GetProductByIdAsync(int id);
+
+        Task<ProductResponseDTO> GetProductsByCategoryAsync(int categoryId);
+
+        Task<ProductResponseDTO> SearchProductAsync(string keyword);
+
+        Task<List<ProductResponseDTO>> FilterProductsAsync(
+            int? categoryId,
+            string? keyword,
+            decimal? minPrice,
+            decimal? maxPrice
+        );
+
+        // Admin functions
+        Task<ProductResponseDTO> CreateProductAsync(ProductCreateDTO dto);
+        Task<ProductResponseDTO> UpdateProductAsync(int id, ProductUpdateDTO dto);
+        Task<bool> DeleteProductAsync(int id);
+
     }
 }
