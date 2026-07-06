8db0b2f3fc314ffc2d002bfcdea7d0076da21c3a
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-07-06 11:46:46 +0700

    Implement product service

--- src/CreationStore.API/Services/Implementations/ProductService.cs
+++ src/CreationStore.API/Services/Implementations/ProductService.cs
@@ -2,6 +2,7 @@ using CreationStore.API.Data;
 using CreationStore.API.DTOs.Products;
 using CreationStore.API.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace CreationStore.API.Services.Implementations
 {
@@ -14,6 +15,12 @@ namespace CreationStore.API.Services.Implementations
             _context = context;
         }
 
+        // ==========================
+        // PUBLIC / MEMBER
+        // ==========================
+
+
+        // Lấy tất cả sản phẩm đang active
         public async Task<List<ProductResponseDTO>> GetAllProductsAsync()
         {
             var products = await _context.Products
@@ -35,6 +42,7 @@ namespace CreationStore.API.Services.Implementations
             return products;
         }
 
+        // Lấy ra chi tiết 1 sản phẩm theo id
         public async Task<ProductResponseDTO?> GetProductByIdAsync(int id)
         {
             var product = await _context.Products
@@ -55,5 +63,187 @@ namespace CreationStore.API.Services.Implementations
 
             return product;
         }
+
+        // Lọc sản phẩm theo category
+        public async Task<List<ProductResponseDTO>> GetProductsByCategoryAsync(int categoryId)
+        {
+            var products = await _context.Products
+                    .AsNoTracking()
+                    .Where(p => p.CategoryId == categoryId && p.IsActive)
+                    .Select(p => new ProductResponseDTO
+                    {
+                        ProductId = p.ProductId,
+                        ProductName = p.ProductName,
+                        Description = p.Description,
+                        Price = p.Price,
+                        ImageUrl = p.ImageUrl,
+                        ValidityDays = p.ValidityDays,
+                        CategoryId = p.CategoryId,
+                        CategoryName = p.Category.CategoryName
+                    })
+                    .ToListAsync();
+            return products;
+        }
+
+        // Tìm kiếm sản phẩm theo tên
+        public async Task<List<ProductResponseDTO>> SearchProductsAsync(string keyword)
+        {
+            keyword = keyword.Trim();
+            var products = await _context.Products
+                    .AsNoTracking()
+                    .Where(p => p.IsActive && p.ProductName.Contains(keyword))
+                    .Select(p => new ProductResponseDTO
+                    {
+                        ProductId = p.ProductId,
+                        ProductName = p.ProductName,
+                        Description = p.Description,
+                        Price = p.Price,
+                        ImageUrl = p.ImageUrl,
+                        ValidityDays = p.ValidityDays,
+                        CategoryId = p.CategoryId,
+                        CategoryName = p.Category.CategoryName
+                    })
+                    .ToListAsync();
+            return products;
+        }
+
+        // Lọc nâng cao: category, keyword, minPrice, maxPrice
+        public async Task<List<ProductResponseDTO>> FilterProductsAync(
+            int? categoryId,
+            string? keyword,
+            decimal? minPrice,
+            decimal? maxPrice
+        )
+        {
+            var query = _context.Products
+                .AsNoTracking()
+                .Where(p => p.IsActive);
+
+            if (categoryId.HasValue)
+            {
+                query = query.Where(p => p.CategoryId == categoryId.Value);
+            }
+
+            if (!string.IsNullOrWhiteSpace(keyword))
+            {
+                keyword = keyword.Trim();
+                query = query.Where(p => p.ProductName.Contains(keyword));
+            }
+
+            if (minPrice.HasValue)
+            {
+                query = query.Where(p => p.Price >= minPrice.Value);
+            }
+
+            if (maxPrice.HasValue)
+            {
+                query = query.Where(p => p.Price <= maxPrice.Value);
+            }
+
+            var products = await query
+                .Select(p => new ProductResponseDTO
+                {
+
+                    ProductId = p.ProductId,
+                    ProductName = p.ProductName,
+                    Description = p.Description,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl,
+                    ValidityDays = p.ValidityDays,
+                    CategoryId = p.CategoryId,
+                    CategoryName = p.Category.CategoryName
+                })
+                .ToListAsync();
+            return products;
+        }
+
+        // ==========================
+        // ADMIN
+        // Sau này thêm [Authorize(Roles = "Admin")] ở Controller
+        // ==========================
+        // Thêm sản phẩm mới
+        public async Task<ProductResponseDTO?> CreateProductAsync(ProductCreateDTO dto)
+        {
+            var categoryExists = await _context.Categories
+                .AnyAsync(c => c.CategoryId == dto.CategoryId && c.IsActive);
+
+            if (!categoryExists)
+            {
+                return null;
+            }
+
+            var product = new Product
+            {
+                ProductName = dto.ProductName,
+                Description = dto.Description,
+                Price = dto.Price,
+                ImageUrl = dto.ImageUrl,
+                ValidityDays = dto.ValidityDays,
+                CategoryId = dto.CategoryId,
+                IsActive = true,
+                CreatedAt = DateTime.Now
+            };
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            var result = await GetProductByIdAsync(product.ProductId);
+
+            return result;
+        }
+        // Cập nhật sản phẩm
+        public async Task<ProductResponseDTO?> UpdateProductAsync(int id, ProductUpdateDTO dto)
+        {
+            var product = await _context.Products
+                .FirstOrDefaultAsync(p => p.ProductId == id && p.IsActive);
+
+            if (product == null)
+            {
+                return null;
+            }
+
+            var categoryExists = await _context.Categories
+                .AnyAsync(c => c.CategoryId == dto.CategoryId && c.IsActive);
+
+            if (!categoryExists)
+            {
+                return null;
+            }
+
+            product.ProductName = dto.ProductName;
+            product.Description = dto.Description;
+            product.Price = dto.Price;
+            product.ImageUrl = dto.ImageUrl;
+            product.ValidityDays = dto.ValidityDays;
+            product.CategoryId = dto.CategoryId;
+            product.UpdatedAt = DateTime.Now;
+
+            await _context.SaveChangesAsync();
+
+            var result = await GetProductByIdAsync(product.ProductId);
+
+            return result;
+        }
+
+        // Xóa mềm sản phẩm
+        // Không xóa khỏi database, chỉ set IsActive = false
+         public async Task<bool> DeleteProductAsync(int id)
+        {
+            var product = await _context.Products
+                .FirstOrDefaultAsync(p => p.ProductId == id && p.IsActive);
+
+            if (product == null)
+            {
+                return false;
+            }
+
+            product.IsActive = false;
+            product.UpdatedAt = DateTime.Now;
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
     }
 }
