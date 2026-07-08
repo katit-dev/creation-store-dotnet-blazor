9b44dc97fcfe6528a0848fa75c122dd93dabea8a
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-07-08 11:32:23 +0700

    crate IService for category

---
+++ src/CreationStore.API/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,25 @@
+using CreationStore.API.DTOs.Categories;
+
+namespace CreationStore.API.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        // ==========================
+        // PUBLIC / MEMBER
+        // ==========================
+
+        Task<List<CategoryResponseDTO>> GetAllCategoriesAsync();
+
+        Task<CategoryResponseDTO?> GetCategoryByIdAsync(int id);
+
+        // ==========================
+        // ADMIN
+        // ==========================
+
+        Task<CategoryResponseDTO?> CreateCategoryAsync(CategoryCreateDTO dto);
+
+        Task<CategoryResponseDTO?> UpdateCategoryAsync(int id, CategoryUpdateDTO dto);
+
+        Task<bool> DeleteCategoryAsync(int id);
+    }
+}
