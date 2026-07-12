a63b31d67aa1e380fd94f6e05eef4849f8a426fc
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-07-12 14:53:50 +0700

    create IAuthService.cs

---
+++ src/CreationStore.API/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,14 @@
+using CreationStore.API.DTOs.Auth;
+using CreationStore.API.DTOs.ResponseTypes;
+
+namespace CreationStore.API.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<ResponseTypeDTO<RegisterResponseDTO>> RegisterAsync(RegisterDTO dto);
+
+        Task<ResponseTypeDTO<LoginResponseDTO>> LoginAsync(LoginDTO dto);
+
+        Task<ResponseTypeDTO<UserProfileDTO>> GetMeAsync(int userId);
+    }
+}
