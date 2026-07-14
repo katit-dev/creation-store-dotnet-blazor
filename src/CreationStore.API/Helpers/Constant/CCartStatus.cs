c12fb11b28bae0f6af6d0255145befb586256995
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-07-14 13:14:01 +0700

    create Cart status constant

---
+++ src/CreationStore.API/Helpers/Constant/CCartStatus.cs
@@ -0,0 +1,9 @@
+namespace CreationStore.API.Helpers.Constant
+{
+    public static class CCartStatus
+    {
+        public const string Active = "Active";
+
+        public const string CheckedOut = "CheckedOut";
+    }
+}
