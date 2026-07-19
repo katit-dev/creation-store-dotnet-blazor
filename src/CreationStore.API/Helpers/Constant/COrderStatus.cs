149e865729f12c08d906d8831661aed0f82f2888
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-07-19 07:05:41 +0700

    create COrderStatus and CPaymentStatus

---
+++ src/CreationStore.API/Helpers/Constant/COrderStatus.cs
@@ -0,0 +1,13 @@
+namespace CreationStore.API.Helpers.Constant
+{
+    public static class COrderStatus
+    {
+        public const string PendingPayment = "PendingPayment";
+
+        public const string Paid = "Paid";
+
+        public const string Completed = "Completed";
+
+        public const string Cancelled = "Cancelled";
+    }
+}
---
+++ src/CreationStore.API/Helpers/Constant/CPaymentStatus.cs
@@ -0,0 +1,13 @@
+namespace CreationStore.API.Helpers.Constant
+{
+    public static class CPaymentStatus
+    {
+        public const string Pending = "Pending";
+
+        public const string Succeeded = "Succeeded";
+
+        public const string Failed = "Failed";
+
+        public const string Cancelled = "Cancelled";
+    }
+}
