33b8892b91472cb730de7936674e28744b055a9b
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-07-26 15:06:10 +0700

    create constant files for payment module

---
+++ src/CreationStore.API/Helpers/Constant/CPaymentMethod.cs
@@ -0,0 +1,7 @@
+namespace CreationStore.API.Helpers.Constant
+{
+    public static class CPaymentMethod
+    {
+        public const string VnPay = "VNPAY";
+    }
+}
---
+++ src/CreationStore.API/Helpers/Constant/CPaymentTransactionStatus.cs
@@ -0,0 +1,13 @@
+namespace CreationStore.API.Helpers.Constant
+{
+    public static class CPaymentTransactionStatus
+    {
+        public const string Pending = "Pending";
+
+        public const string Succeeded = "Succeeded";
+
+        public const string Failed = "Failed";
+
+        public const string Cancelled = "Cancelled";
+    }
+}
