9a56723c625348675ffdb78ea7572b468bb9dfbd
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-08-11 02:15:32 +0700

    add tokenStorage

---
+++ src/CreationStore.Blazor/Helpers/TokenStorage.cs
@@ -0,0 +1,41 @@
+using Microsoft.JSInterop;
+
+namespace CreationStore.Blazor.Helpers
+{
+    public class TokenStorage
+    {
+        private const string TokenKey = "creation_store_token";
+
+        private readonly IJSRuntime _jsRuntime;
+
+        public TokenStorage(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task SaveTokenAsync(string token)
+        {
+            await _jsRuntime.InvokeVoidAsync(
+                "localStorage.setItem",
+                TokenKey,
+                token
+            );
+        }
+
+        public async Task<string?> GetTokenAsync()
+        {
+            return await _jsRuntime.InvokeAsync<string?>(
+                "localStorage.getItem",
+                TokenKey
+            );
+        }
+
+        public async Task RemoveTokenAsync()
+        {
+            await _jsRuntime.InvokeVoidAsync(
+                "localStorage.removeItem",
+                TokenKey
+            );
+        }
+    }
+}
